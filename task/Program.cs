76233ec9a21a654dc6333ba0bd73fa5e8dc7242a
Anna <atsigip@gmail.com>
2022-01-29 15:14:04 +0300

    task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] myArray;
+int count;
+Console.Write("count = ");
+count = Int32.Parse(Console.ReadLine());
+myArray = new string[count];
+Console.WriteLine("Введите массив:");
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write("myArray[{0}] = ", i);
+    myArray[i] = Console.ReadLine();
+}
+
+string[] newArray = new string[myArray.Length];
+int j = 0;
+Console.Write("Новый массив: ");
+for (int i = 0; i < myArray.Length; i++)
+ {
+    if (myArray[i].Length < 4 && myArray[i].Length > 0)
+    {
+        newArray[i] = myArray[i];
+        j++;
+
+         Console.Write($"{newArray[i]} ");
+    }
+}
